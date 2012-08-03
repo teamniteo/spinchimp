d4e6b106c2bbe42957c4961f8b3151fa635ed65b
Matej Cotman <matejc@kiberpipa.org>
2012-08-03 12:23:12 +0200

    setup files added

---
+++ setup.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""Installer for this package."""
+
+from setuptools import find_packages
+from setuptools import setup
+
+import os
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+version = read('src', 'spinchimp', 'version.txt').strip()
+
+long_description = \
+    read('README.rst') + \
+    read('docs', 'HISTORY.rst') + \
+    read('docs', 'LICENSE.rst')
+
+setup(
+    name='spinchimp',
+    version=version,
+    description="Python bindings for SpinChimp API",
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP",
+    ],
+    keywords='API spinner SpinChimp',
+    author='NiteoWeb Ltd.',
+    author_email='info@niteoweb.com',
+    url='http://www.niteoweb.com',
+    license='BSD',
+    packages=find_packages('src', exclude=['ez_setup']),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+    ],
+    extras_require={
+        # list libs needed for unittesting this project
+        'test': [
+            'mock',
+            'unittest2',
+        ],
+        # list libs needed for releasing this project
+        'release': [
+            'zest.releaser',   # bin/longtest
+            'jarn.mkrelease',  # bin/mkrelease
+        ],
+    },
+)
